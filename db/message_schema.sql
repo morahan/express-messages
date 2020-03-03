7c1b2ea793f98bbefcdc96f27b6aecf04f87dddf
Michael Morahan <morahan.mike@gmail.com>
2020-03-03 12:56:50 -0700

    create database and populate with two default messages.

--- db/message_schema.sql
+++ db/message_schema.sql
@@ -11,8 +11,20 @@ CREATE DATABASE messages;
 \c messages;
 
 CREATE TABLE messages (
--- build your schema here
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(47),
+  message VARCHAR(247)
 );
+
+Insert into messages (name, message) values ('mike', 'this is the first message in my database');
+Insert into messages (name, message) values ('mo', 'second message');
 
 -- Seed your data with a collection of insert statements
 -- INSERT INTO messages () VALUES ();
+
+
+-- psql -U dbuser -d dbname -a -f filename.sql --> 
+-- psql -U postgres --> to login as default user
+-- \i /Users/mmorahan/Desktop/Hack/realCourse/mini-apps/express-messages/db/message_schema.sql
+-- \i <absolute file path> --> run file from this location
+-- \c postgres --> connect out to postgress
