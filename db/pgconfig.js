8b6f2927ef5c0140696bc60bb1ffad162b9582ec
Michael Morahan <morahan.mike@gmail.com>
2020-03-03 19:37:08 -0700

    Solved basic requirements.  Finally things are clicking.
    writing connections between server and database.

--- db/pgconfig.js
+++ db/pgconfig.js
@@ -1,6 +1,49 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  database: 'messages'
+  database: 'messages',
+  user: 'postgres',
+  password: '1234'
 });
 
-module.exports = pool;
+function allMessages() {
+  return pool.query('select * from messages')
+    .then(data => {
+      return data.rows;
+    }) 
+}
+
+function writeMessage(arr) {
+  return pool.query('insert into messages (name, message) values ($1, $2)', arr)
+  .then(result => {
+    return 'wrote message to database';
+  })
+}
+
+function updateMessage(arr){
+  return pool.query('update messages set name = $2 ,message = $3  where id = $1', arr)
+  .then(result => {
+    return 'successfully updated message';
+  })
+}
+
+function deleteMessage(arr){
+  return pool.query('delete from messages where id =$1', arr)
+  .then(result => {
+    return 'message has been lazered into space!'
+  } )
+}
+
+function oneMessage(arr){
+  return pool.query('select * from messages where id=$1', arr)
+  .then(result => {
+    return result.rows;
+  })
+}
+
+module.exports = {
+  allMessages, 
+  writeMessage,
+  updateMessage,
+  deleteMessage,
+  oneMessage
+}
--- server.js
+++ server.js
@@ -1,20 +1,56 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const dbMethods = require('./db/pgconfig');
+const body = require('body-parser');
 
 // if you are using mongo/mongoose uncomment this line
 // const Message = require('./db/Message');
 
 // if you are using postgres, uncomment this line
-// const pool = require('./db/pgconfig');
+app.use(body.json());
+
+// get all messages
+app.get('/api/messages', (req, res) => {
+  dbMethods.allMessages()
+  .then(result => res.send(result));
+})
+
+// add message
+app.post('/api/messages', (req, res) => {
+  let arr = [req.body.name, req.body.message];
+  dbMethods.writeMessage(arr)
+  .then(data => res.sendStatus(201));
+})
+
+// update message
+app.put('/api/messages/:id', (req, res) => {
+  let arr = [req.params.id, req.body.name, req.body.message]
+  dbMethods.updateMessage(arr)
+  .then(result => res.send(result))
+})
+
+//delete message
+app.delete('/api/messages/:id', (req, res) => {
+  let arr = [req.params.id];
+  dbMethods.deleteMessage(arr)
+  .then(result => res.send(result))
+})
+
+// get one specific message
+app.get('/api/messages/:id', (req, res) => {
+  let arr = [req.params.id];
+  dbMethods.oneMessage(arr)
+  .then(result => res.send(result))
+})
+
 
 app.use((req,res,next) => {
-  res.status(404).send('That route does not exist');
+  res.status(404).send('That route does not exist Bailey!');
 });
 
-const port = 3000;
-
 app.listen(port, () => {
-  console.log('Listening on port', port);
+  console.log('Listening on port, 123', port);
 });
 
 module.exports = app;
